b3d4bfb423fa7c2e341bf08ae034409542ff52d7
Ubuntu <ubuntu@ip-172-31-14-224.ec2.internal>
2022-08-06 18:39:17 +0000

    project 4

--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath="hardeydotun/microserviceproject:v1.0.0"
 
 # Step 2:  
 # Authenticate & tag
+docker login
+docker tag microserviceproject ${dockerpath}
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker push ${dockerpath}
